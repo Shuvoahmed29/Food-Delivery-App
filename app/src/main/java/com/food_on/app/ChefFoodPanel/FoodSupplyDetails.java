ade99e2897f93aead34313a6a3ba78fc03b55a4c
Md.Raihanul Haque(Shuvo) <raihanulhaque1170@gmail.com>
2023-08-07 17:52:52 +0600

    All File
    Food Delivery Apps Code.

---
+++ app/src/main/java/com/food_on/app/ChefFoodPanel/FoodSupplyDetails.java
@@ -0,0 +1,18 @@
+package com.food_on.app.ChefFoodPanel;
+
+public class FoodSupplyDetails {
+
+    public String Dishes,Quantity,Price,Description,ImageURL,RandomUID,ChefId;
+
+    public FoodSupplyDetails(String dishes, String quantity, String price, String description, String imageURL,String randomUID,String chefId) {
+        Dishes = dishes;
+        Quantity = quantity;
+        Price = price;
+        Description = description;
+        ImageURL = imageURL;
+        if(ImageURL==null) ImageURL="hello";
+        RandomUID=randomUID;
+        ChefId=chefId;
+    }
+
+}
